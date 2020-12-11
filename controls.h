88d4a57ab2fc24d305e033f1b2b6199af58095cc
Vikneshwar Thandeswaran <tvikneshwar@gmail.com>
2020-12-11 19:32:34 +0530

    Update controls.h

--- controls.h
+++ controls.h
@@ -15,15 +15,18 @@ valve1open()
       //MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
 
       printf("open N1\n");
+      
       //MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
-
+      
       printf("open v1\n");
+      status (1 , 1 ); // status followed by valve number(location
       //MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
 
       printf("close n0\n");
       //MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
 
       printf("close v2\n");
+      status (1 , 1 ); // status followed by valve number(location
       //MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
     
 }
