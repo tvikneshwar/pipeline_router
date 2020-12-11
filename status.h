6ff7529a12352b24e8b49ad762818e6082a87423
Vikneshwar Thandeswaran <tvikneshwar@gmail.com>
2020-12-11 19:22:07 +0530

    Update status.h

--- status.h
+++ status.h
@@ -1,11 +1,27 @@
-/*status*/
 
-int status()
-{
+ int status(int status, int address )
+ {
+     int S=status;
+     int L=address;            //location
+     
+     
+     
+//               v0  ,v1  ,v2 ,v3
+    int stat[] = {0,  0,  0,  0}; 
+    printf("befor updation: ");
+    printf("%d", stat[L]);
+    
+    stat[L] = S;
+    printf("status updated sucessfully.^.\n");
+    printf("status: ");
+    printf("%d", stat[L]);
 
-
-
-
-
-
-}
+ }
+ 
+ int main()
+ {
+     
+    status (1 , 1 ); // status followed by valve number(location)
+     
+    
+ }
