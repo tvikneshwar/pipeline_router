0370035eb2cc2fd79fe11e65cb8e6bcbcbc5806c
Vikneshwar Thandeswaran <tvikneshwar@gmail.com>
2020-12-11 19:21:40 +0530

    Update controls.h

--- controls.h
+++ controls.h
@@ -1,4 +1,5 @@
 /*controls */
+#include"status.h"
 int v1o();
 int v1c();
 int v2o();
@@ -10,18 +11,22 @@ int cmd = 1;
 valve1open()
     {
       printf("open N0\n");
-      MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
+      
+      //MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
 
       printf("open N1\n");
-      MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
+      //MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
 
       printf("open v1\n");
-      MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
+      //MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
 
       printf("close n0\n");
-      MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
+      //MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
 
       printf("close v2\n");
-      MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
+      //MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
     
-   }
+}
+    
+    
+    
